678322ad8dcc82e7850e0bc9c103a378cc865a47
Loïs Ramanandraibe <lois.ramanandraibe@gmail.com>
2021-02-17 20:18:34 +0100

    refactorisation

--- js/formValidation.js
+++ js/formValidation.js
@@ -5,79 +5,119 @@ const email = document.getElementById('email');
 const birthdate = document.getElementById('birthdate');
 const tournamentQuantity = document.getElementById('quantity');
 const form = document.getElementById('form');
+const inputsToVerify = document.querySelectorAll('input');
+const locationSelector = document.getElementsByName("location");
+const errorCityText = document.getElementById('errorCity');
+const checkboxConditions = document.getElementById('checkbox1');
+const errorConditions = document.getElementById('errorConditionsText');
+const cities = document.getElementById('cities');
+const conditions = document.getElementById('conditions');
 
-//REGEX
-let nameRegex = /^[a-zA-ZÀ-ÖØ-öø-ÿ-]+$/;
-let emailRegex = /\S+@\S+\.\S+/;
+// fonctions validations
+function birthdateOK() {
+   if (birthdate.value < birthdate.min || birthdate.value > birthdate.max) {
+      return false;
+   } else {
+      return true;
+   }
+};
 
+function tournamentsOK() {
+   if (tournamentQuantity.value < tournamentQuantity.min || tournamentQuantity.value > tournamentQuantity.max) {
+      return false;
+   } else {
+      return true;
+   }
+};
 
-// VALIDATIONS INPUTS
+// FONCTION VERIFYINPUT avec switch(inputType)
+let valide = false; // la façon que j'ai trouvé pour que la fonction me retourne un bool si tout les cases sont valides, change en fonction de test.
 
-// check prénom
-function checkFirstName() {
-    if (firstName.value.length < 2 || firstName.value === '' || !firstName.value.match(nameRegex)) { // si l'input est vide, ou a moins de 2 caractères
-        firstName.style.border = '2px solid #e54858';  // le border devient rouge
-        firstName.nextElementSibling.style.display = "block"; // et un message d'erreur s'affiche en dessous
-        return false;
-    }else {
-        firstName.style.border = '2px solid #279e7a'; // si l'input à 2 caractère ou plus, le border devient vert.
-        firstName.nextElementSibling.style.display = "none"; //pas de message d'erreur.
-        return true;
-    }
+function verifyInput (input) {
+    const inputType = input.getAttribute('type');
+    const nameRegex = /^[a-zA-ZÀ-ÖØ-öø-ÿ-]{2,}/;
+    const emailRegex = /\S+@\S+\.\S{2,}/;
+    let test;
+    
+ 
+ switch(inputType) {
+     case "text" :
+         test = nameRegex.test(input.value);
+         break;
+     case "email":
+         test = emailRegex.test(input.value);
+         break;
+     case "date":
+         test = birthdateOK();
+         break;
+      case "number":
+         test = tournamentsOK();
+         break;
+ }    
+    if (test) {
+        input.style.border = '2px solid green';
+        input.nextElementSibling.style.display = "none";
+        valide = true 
+     } 
+     else {
+        input.style.border = '2px solid red';
+        input.nextElementSibling.style.display = "block";
+        valide = false
+     }
+
+     console.log(valide);
 }
 
-//check nom de famille
-function checkLastName() {
-    if (lastName.value.length < 2 || lastName.value === '' || !lastName.value.match(nameRegex)) {
-        lastName.style.border = '2px solid #e54858';
-        lastName.nextElementSibling.style.display = "block";
-        return false
-    }else {
-        lastName.style.border = '2px solid #279e7a';
-        lastName.nextElementSibling.style.display = "none";
-        return true;
-    }
+inputsToVerify.forEach( input => {
+   input.addEventListener('change', () => {
+      verifyInput(input)
+   });
+});
+
+// FONCTIONS + AddEventListener pour les checkbox.
+
+function cityOk(){
+   let check1 = false;
+   for(i=0; i<locationSelector.length; i++) { // On observe les inputs pour savoir s'il y en a un de checked
+       if (locationSelector[i].checked) {
+          check1 = true; // si une check alors check1 = true
+       }
+     }
+       if (check1 == false) { // si check1 est = false alors le message d'erreur s'affiche
+            errorCityText.style.display = "block"
+            return false;
+       }else {
+            errorCityText.style.display = "none"
+            return true;
+       }
 }
 
-//check email
-function checkEmail() {
-    if (email.value.match(emailRegex)) { // si l'email est valide, match le Regex, 
-        email.style.border = '2px solid #279e7a'; // le border est vert
-        email.nextElementSibling.style.display = "none"; // pas de message d'erreur
-        return true
-    }else {
-        email.style.border = '2px solid #e54858'; // sinon le border est rouge et le message d'erreur s'affiche
-        email.nextElementSibling.style.display = "block";
-        return false;
-    }
+cities.addEventListener('change', () => {
+   cityOk()
+});
+
+function conditionsOk() {
+   if (!checkboxConditions.checked) {
+       errorConditionsText.style.display = 'block';
+       return false;
+   } else {
+       errorConditionsText.style.display = 'none';
+       return true;
+   }
 }
 
-//check date de naissance
-function checkBirthdate() {
-    if (birthdate.value < birthdate.min || birthdate.value > birthdate.max) { //si la date de naissance ne se trouve pas entre le min et le max définit dans le HTML
-        birthdate.style.border = '2px solid #e54858'; // le border est rouge
-        birthdate.nextElementSibling.style.display = "block"; // le message d'erreur s'affiche
-        return false
-    }else {
-        birthdate.style.border = '2px solid #279e7a'; // si la date est valide
-        birthdate.nextElementSibling.style.display = "none"; // le border est vert et pas de message d'erreur
-        return true;
-    }
-}
+conditions.addEventListener('change', () => {
+   conditionsOk()
+});
 
+ form.addEventListener('submit', (e) => { // Donc si Valide est true tout est ok sinon pas ok. 
+   //Sauf que si un seul champ est valide et les autres ne sont pas remplis, ou le dernier champ rempli est valide l'alert OK s'affiche.
+        e.preventDefault();
+        if (valide === true) {
+         alert("OKAY")
+        } else {
+         alert("NOT OKAY")
+        };
 
-// Ecoute de l'évèvement 'SUBMIT' du formulaire
-form.addEventListener('submit', (e) => {
-    e.preventDefault();
-    if(checkFirstName() === true && checkLastName() === true && checkEmail() === true && checkBirthdate() === true) { //si les inputs prénom & nom sont valides, une alerte s'affiche
-        alert("OKAY")
-    } else {
-        checkFirstName();
-        checkLastName();
-        checkEmail();
-        checkBirthdate();
-    }
-
-})
-
+ })
 
